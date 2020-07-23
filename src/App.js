2b8fe1276475860445c06cf1d7785e59d10567a9
Akasho2h <akash.sharma@o2h.com>
2020-07-23 22:22:43 +0530

    Basic Redux based Cake shop application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import UserList from './Users/User'
 import Appcss from './App.css'
+import { Provider } from 'react-redux'
+import store from './Redux/store'
+// import UserList from './components/Users/User'
+import CakeContainer from './components/ReduxDemo/CakeContainer'
 const App = () => {
 
     return(
-        <>
-            <div className="home">
-                <UserList/>
-            </div>
-        </>
+        <Provider store={store}>
+            <>
+                <div className="home">
+                    {/* <UserList/> */}
+                    <CakeContainer/>
+                </div>
+            </>
+        </Provider>
     )
 
 }
