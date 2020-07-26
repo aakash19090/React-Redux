67c1dc28dd0c7db00271f1941a87eeed2b85a961
Akasho2h <akash.sharma@o2h.com>
2020-07-26 15:47:58 +0530

    applied logger middleware

--- src/components/ReduxDemo/CakeContainer.js
+++ src/components/ReduxDemo/CakeContainer.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { buyCake } from '../../Redux/index'
 function CakeContainer(props) {
-    console.log(props)
     return (
         <div>
             <h1>Number Of Cakes - {props.numOfCakes}</h1>
